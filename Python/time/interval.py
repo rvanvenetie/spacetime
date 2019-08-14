7fdc554f5569e601fb6b903e6fa7c24d77a04784
Raymond van Venetie <rayvanve@gmail.com>
2019-08-14 14:53:56 +0200

    let interval make use of rational numbers

--- Python/time/interval.py
+++ Python/time/interval.py
@@ -1,9 +1,16 @@
-import collections.abc
+from fractions import Fraction
 
 
-class Interval(collections.namedtuple('Interval', 'a b')):
+class Interval(object):
     """ Represents an open or closed interval (a,b). """
-    __slots__ = ()
+    __slots__ = ('a','b')
+
+    def __init__(self, a, b):
+        print(type(a))
+        assert isinstance(a, int) or isinstance(a, Fraction)
+        assert isinstance(b, int) or isinstance(b, Fraction)
+        self.a = Fraction(a)
+        self.b = Fraction(b)
 
     @property
     def mid(self):
@@ -30,12 +37,12 @@ class Interval(collections.namedtuple('Interval', 'a b')):
 
 class IntervalSet(object):
     """ Represents a collection of intervals with fast merging of overlap.
-    
+
     """
 
     def __init__(self, intervals):
         """ This runs in O(N log N) time.
-        
+
         Current complexity: O(N log N) because of a sort step
         Goal complexity: O(N)
         TODO: if we assume to receive a list of intervals that is already sorted
@@ -49,7 +56,7 @@ class IntervalSet(object):
         sorted_intervals = sorted(intervals, key=lambda interval: interval.a)
         stack = []
         if len(sorted_intervals):
-            stack.append(Interval(*sorted_intervals[0]))
+            stack.append(Interval(sorted_intervals[0].a, sorted_intervals[0].b))
             for interval in sorted_intervals:
                 if interval.intersects(stack[-1], closed=True):
                     stack[-1] = Interval(stack[-1].a,
@@ -59,6 +66,9 @@ class IntervalSet(object):
             self.intervals = stack
         else:
             self.intervals = []
+
+    def __iter__(self):
+        return iter(self.intervals)
 
     def covers(self, interval):
         """ Test if the interval set contains the given interval completely."""
--- Python/time/interval_test.py
+++ Python/time/interval_test.py
@@ -1,18 +1,21 @@
+from fractions import Fraction
 from interval import Interval, IntervalSet
 
 
+def F(x,y): return Fraction(x,y)
+
 def test_interval_intersect():
-    assert not Interval(0, 0.5).intersects(Interval(0.75, 1.0))
-    assert not Interval(0.75, 1.0).intersects(Interval(0, 0.5))
-    assert not Interval(0.5, 1.0).intersects(Interval(0, 0.5), closed=False)
-    assert Interval(0.5, 1.0).intersects(Interval(0, 0.5), closed=True)
-    assert not Interval(0, 0.25).intersects(Interval(0.5, 1.0))
+    assert not Interval(0, F(1,2)).intersects(Interval(F(3,4), 1))
+    assert not Interval(F(3,4), 1).intersects(Interval(0, F(1,2)))
+    assert not Interval(F(1,2), 1).intersects(Interval(0, F(1,2)), closed=False)
+    assert Interval(F(1,2), 1).intersects(Interval(0, F(1,2)), closed=True)
+    assert not Interval(0, F(1,4)).intersects(Interval(F(1,2), 1))
 
 
 def test_interval_merge():
     ivs = IntervalSet(
         [Interval(0, 1),
-         Interval(0, 0.5),
+         Interval(0, F(1,2)),
          Interval(0, 1),
-         Interval(0, 0.5)])
+         Interval(0, F(1,2))])
     assert ivs.covers(Interval(0, 1))
