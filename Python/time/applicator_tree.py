33de904062f20c0913f2154f55ab9b5fffe2abdb
Raymond van Venetie <rayvanve@gmail.com>
2019-08-22 19:09:47 +0200

    added new implementation, using mainly tree structures.

---
+++ Python/time/applicator_tree.py
@@ -0,0 +1,239 @@
+from indexed_vector import IndexedVector
+from interval import Interval, IntervalSet
+from basis_tree import Element, HaarScaling
+
+
+
+class Applicator(object):
+    """ Class that can apply multiscale operators with minimal overhead.
+
+    See also github.com/skestler/lawa-phd-skestler/LAWA-lite/
+        lawa/methods/adaptive/operators/localoperators/localoperator1d.{h,tcpp}
+    for some (tough to read) C++ code that implements the same operators in
+    a probably more optimized fashion.
+    """
+
+    def __init__(self,
+                 basis_in,
+                 singlescale_operator,
+                 Lambda_in=None,
+                 basis_out=None,
+                 Lambda_out=None):
+        """ Initialize the applicator.
+
+        Arguments:
+            basis_in: A Basis object.
+            singlescale_operator: LinearOperator from basis_in to basis_out.
+            Lambda_in: multiscale indices to apply the multiscale operator on
+                (default: the full set of indices of basis_in).
+            basis_out: A Basis object (default: basis_in).
+            Lambda_out: multiscale indices to compute application on
+                (default: value of Lambda_in)
+        """
+        self.basis_in = basis_in
+        self.basis_out = basis_out if basis_out else basis_in
+        self.operator = singlescale_operator
+        self.Lambda_in = Lambda_in if Lambda_in else basis_in.indices
+        self.Lambda_out = Lambda_out if Lambda_out else self.Lambda_in
+
+
+    def _initialize_elements(self):
+        """ Helper function to set correct fields inside the elements. """
+
+        def reset(elem):
+            """ Reset the variables! :-) """
+            elem.Lambda_in = False
+            elem.Lambda_out = False
+            elem.Pi_in = False
+            elem.Pi_out = False
+            for child in elem.children:
+                reset(child)
+        reset(Element.mother_element)
+
+        for psi in self.Lambda_in:
+            for elem in list(psi.support):
+                elem.Lambda_in = True
+
+        for psi in self.Lambda_out:
+            for elem in list(psi.support):
+                elem.Lambda_out = True
+
+    def apply(self, vec):
+        """ Apply the multiscale operator.
+
+        Arguments:
+            vec: an IndexedVector with indices on self.Lambda_in.
+
+        Returns:
+            self.operator(Psi_{Lambda_in})(Psi_{Lambda_out}) vec.
+        """
+        self._initialize_elements()
+        e, f = self._apply_recur(l=1,
+                                 Pi_in=self.Lambda_in.on_level(0),
+                                 Pi_out=self.Lambda_out.on_level(0),
+                                 d=vec.restrict(self.Lambda_in.on_level(0)),
+                                 c=vec)
+        return e + f
+
+    def apply_upp(self, vec):
+        """ Apply the upper part of the multiscale operator.
+
+        Arguments:
+            vec: an IndexedVector with indices on self.Lambda_in.
+
+        Returns:
+            Upper part of self.operator(Psi_{Lambda_in})(Psi_{Lambda_out}) vec.
+        """
+        self._initialize_elements()
+        e, f = self._apply_upp_recur(l=1,
+                                     Pi_in=self.Lambda_in.on_level(0),
+                                     Pi_out=self.Lambda_out.on_level(0),
+                                     d=vec.restrict(self.Lambda_in.on_level(0)),
+                                     c=vec)
+        return e + f
+
+    def apply_low(self, vec):
+        """ Apply the lower part of the multiscale operator.
+
+        Arguments:
+            vec: an IndexedVector with indices on self.Lambda_in.
+
+        Returns:
+            Lower part of self.operator(Psi_{Lambda_in})(Psi_{Lambda_out}) vec.
+        """
+        self._initialize_elements()
+        f = self._apply_low_recur(l=1,
+                                  Pi_in=self.Lambda_in.on_level(0),
+                                  d=vec.restrict(self.Lambda_in.on_level(0)),
+                                  c=vec)
+        return f
+
+    #  Private methods from here on out.
+    def _construct_Pi_out(self, Pi_out):
+        """ Singlescale indices labda st supp(phi_labda) intersects Lambda_l.
+
+        Finds the subset Pi_B of Pi such that the support of phi_labda
+        intersects some S(mu) for mu in Lambda_l.
+        Necessary for computing Pi_B in apply/apply_upp/apply_low.
+
+        Arguments:
+            Pi_out: SingleLevelIndexSet of singlescale indices on level l-1.
+
+        Returns:
+            Pi_B_out: SingleLevelIndexSet of singlescale indices on level l-1.
+        """
+        Pi_B_out = []
+        Pi_A_out = []
+        for phi in Pi_out:
+            # Check the support of phi on level l for wavelets psi.
+            if any((child.Lambda_in for elem in phi.support for child in elem.children)):
+                Pi_B_out.append(phi)
+            else:
+                Pi_A_out.append(phi)
+
+        return Pi_B_out, Pi_A_out
+
+    def _construct_Pi_in(self, Pi_in, Pi_B_out):
+        """ Similar to previous method, only with extra `Pi_B_out`. """
+        Pi_B_in = []
+        Pi_A_in = []
+
+        # Set all the Pi_B_out boys.
+        for phi in Pi_B_out:
+            for elem in phi.support:
+                elem.Pi_out = True
+
+        for phi in Pi_in:
+            # Check if intersects with Phi_out_Pi_B on level l - 1, or
+            # with Lambda_l_out on level l.
+            if any((elem.Pi_out for elem in phi.support)) or \
+                 any((child.Lambda_out for elem in phi.support for child in elem.children)):
+                Pi_B_in.append(phi)
+            else:
+                Pi_A_in.append(phi)
+
+        # Unset all the Pi_B_out boys
+        # Set all the Pi_B_out boys.
+        for phi in Pi_B_out:
+            for elem in phi.support:
+                elem.Pi_out = False
+
+        #TODO: The above might not be neccessary.
+        return Pi_B_in, Pi_A_in
+
+    def _apply_recur(self, l, Pi_in, Pi_out, d, c):
+        """ Apply the multiscale operator on level l.
+
+        Arguments:
+            l: the current level
+            Pi_in: a collection of singlescale indices on level l-1.
+            Pi_out: a collection of singlescale indices on level l-1.
+            d: a vector in ell_2(Pi_in).
+            c: a vector in ell_2(Lambda_{l up}_in).
+
+        Output:
+            e: a vector in ell_2(Pi_bar_out)
+            f: a vector in ell_2(Lambda_{l+1 up}_out).
+        """
+        Lambda_l_in = self.Lambda_in.on_level(l)
+        Lambda_l_out = self.Lambda_out.on_level(l)
+        if len(Pi_out) + len(Lambda_l_out) > 0 and len(Pi_in) + len(
+                Lambda_l_in) > 0:
+            Pi_B_out, Pi_A_out = self._construct_Pi_out(Pi_out)
+            Pi_B_in, Pi_A_in = self._construct_Pi_in(Pi_in, Pi_B_out)
+
+            Pi_bar_out = self.basis_out.P.range(Pi_B_out) | self.basis_out.Q.range(Lambda_l_out)
+            Pi_bar_in = self.basis_in.P.range(Pi_B_in) | self.basis_in.Q.range(Lambda_l_in)
+
+            d_bar = self.basis_in.P.matvec(Pi_B_in, Pi_bar_in, d) + self.basis_in.Q.matvec(Lambda_l_in, Pi_bar_in, c)
+            e_bar, f_bar = self._apply_recur(l + 1, Pi_bar_in, Pi_bar_out, d_bar, c)
+
+            e = self.operator.matvec(Pi_in, Pi_A_out, d) + self.basis_out.P.rmatvec(Pi_bar_out, Pi_B_out, e_bar)
+            f = self.basis_out.Q.rmatvec(Pi_bar_out, Lambda_l_out, e_bar) + f_bar
+            return e, f
+        else:
+            return IndexedVector.Zero(), IndexedVector.Zero()
+
+    def _apply_upp_recur(self, l, Pi_in, Pi_out, d, c):
+        Lambda_l_in = self.Lambda_in.on_level(l)
+        Lambda_l_out = self.Lambda_out.on_level(l)
+        if len(Pi_out) + len(Lambda_l_out) > 0 and len(Pi_in) + len(
+                Lambda_l_in) > 0:
+            Pi_B_out = self._construct_Pi_B_out(Pi_out)
+            Pi_A_out = Pi_out - Pi_B_out
+
+            Pi_bar_out = SingleLevelIndexSet(self.basis_out.P.range(Pi_B_out) | self.basis_out.Q.range(Lambda_l_out))
+            Pi_bar_in = SingleLevelIndexSet(self.basis_in.Q.range(Lambda_l_in))
+
+            d_bar = self.basis_in.Q.matvec(Lambda_l_in, Pi_bar_in, c)
+            e_bar, f_bar = self._apply_upp_recur(l + 1, Pi_bar_in, Pi_bar_out,
+                                                 d_bar, c)
+            e = self.operator.matvec(Pi_in, Pi_out, d) + \
+                self.basis_out.P.rmatvec(Pi_bar_out, Pi_B_out, e_bar)
+            f = self.basis_out.Q.rmatvec(Pi_bar_out, Lambda_l_out,
+                                         e_bar) + f_bar
+            return e, f
+        else:
+            return IndexedVector.Zero(), IndexedVector.Zero()
+
+    def _apply_low_recur(self, l, Pi_in, d, c):
+        Lambda_l_in = self.Lambda_in.on_level(l)
+        Lambda_l_out = self.Lambda_out.on_level(l)
+        if len(Lambda_l_out) > 0 and len(Pi_in) + len(Lambda_l_in) > 0:
+            Pi_B_in = self._construct_Pi_B_in(Pi_in, Pi_B_out={})
+            Pi_bar_in = SingleLevelIndexSet(self.basis_in.P.range(Pi_B_in) | self.basis_in.Q.range(Lambda_l_in))
+            Pi_B_bar_in = SingleLevelIndexSet(self.basis_in.P.range(Pi_B_in))
+            Pi_B_bar_out = SingleLevelIndexSet(self.basis_out.Q.range(Lambda_l_out))
+
+            # NB: operator is applied at level `l` -- different from the rest.
+            e_bar = self.operator.matvec(
+                Pi_B_bar_in, Pi_B_bar_out,
+                self.basis_in.P.matvec(Pi_B_in, Pi_B_bar_in, d))
+            d_bar = self.basis_in.P.matvec(Pi_B_in, Pi_bar_in, d) + \
+                    self.basis_in.Q.matvec(Lambda_l_in, Pi_bar_in, c)
+            f = self.basis_out.Q.rmatvec(Pi_B_bar_out, Lambda_l_out,
+                                         e_bar) + self._apply_low_recur(
+                                             l + 1, Pi_bar_in, d_bar, c)
+            return f
+        else:
+            return IndexedVector.Zero()
---
+++ Python/time/basis_tree.py
@@ -0,0 +1,392 @@
+from linear_operator import LinearOperator
+from interval import Interval
+from fractions import Fraction
+
+def support_to_interval(lst):
+    """ Convert a list of tuples (l, n) to an actual interval.
+
+    TODO: Legacy, remove this. """
+    return Interval(lst[0].interval.a, lst[-1].interval.b)
+
+class MultiscaleIndices:
+    """ Immutable set of multiscale indices.  """
+
+    def __init__(self, indices):
+        self.indices = indices
+        self.maximum_level = max([fn.labda[0] for fn in indices])
+        self.per_level = [[] for _ in range(self.maximum_level + 1)]
+        for fn in indices:
+            self.per_level[fn.labda[0]].append(fn)
+
+    def __repr__(self):
+        return r"MSIS(%s)" % self.indices
+
+    def __len__(self):
+        return len(self.indices)
+
+    def __iter__(self):
+        return self.indices.__iter__()
+
+    def __next__(self):
+        return self.indices.__next__()
+
+    def __contains__(self, index):
+        return self.indices.__contains__(index)
+
+    def on_level(self, level):
+        if level > self.maximum_level: return []
+        return self.per_level[level]
+
+    def until_level(self, level):
+        """ Expensive (but linear in size) method. Mainly for testing. """
+        return MultiscaleIndices(
+            {index
+             for index in self.indices if index.labda[0] <= level})
+
+class Element:
+    def __init__(self, level, node_index, parent):
+        self.level = level
+        self.node_index = node_index
+        self.parent = parent
+        self.children = []
+
+        # Add some extra variables necessary for applicator.
+        # This should definitely be somewhere else..
+        self.Lambda_in = False
+        self.Lambda_out = False
+        self.Pi_in = False
+        self.Pi_out = False
+
+    def bisect(self):
+        if self.children: return
+        child_left = self.__class__(self.level + 1, self.node_index * 2, self)
+        child_right = self.__class__(self.level + 1, self.node_index * 2 + 1, self)
+        self.children = [child_left, child_right]
+
+    @property
+    def interval(self):
+        h = Fraction(1, 2**self.level)
+        return Interval(h * self.node_index, h * (self.node_index + 1))
+
+    def __repr__(self):
+        return 'Element({}, {})'.format(self.level, self.node_index)
+
+# Initializes a mother_element to be used for all bases.
+Element.mother_element = Element(0, 0, None)
+
+class BaseScaling:
+    def __init__(self, labda, parents, support):
+        self.labda = labda
+        self.parents = parents
+        self.support = support # Support is a list.
+        self.multi_scale = [] # Tranpose of the wavelet to multiscale.
+
+    def prolongate(self):
+        """ Returns a list of pairs with the corresponding coefficients. """
+        pass
+
+    def restrict(self):
+        """ The adjoint of transposing. """
+        pass
+
+    def mass(self):
+        """ Mass inproduct for this scaling element. """
+        pass
+
+    def __repr__(self):
+        return "{}({}, {})".format(self.__class__.__name__, *self.labda)
+
+class HaarScaling(BaseScaling):
+    def __init__(self, labda, parents, support):
+        super().__init__(labda, parents, support)
+        self.children = []
+
+    def refine(self):
+        if self.children: return
+        self.support[0].bisect()
+        l, n = self.labda
+        self.children.append(HaarScaling((l+1, 2*n), self, [self.support[0].children[0]]))
+        self.children.append(HaarScaling((l+1, 2*n+1), self, [self.support[0].children[1]]))
+
+    def prolongate(self):
+        self.refine()
+        return [(self.children[0], 1), (self.children[1], 1)]
+
+    def restrict(self):
+        return [(self.parents, 1)]
+
+    def mass(self):
+        """ The singlescale Haar mass matrix is simply 2**-l * Id. """
+        l, n = self.labda
+        return [(self, 2**-l)]
+
+    @staticmethod
+    def eval_mother(x):
+        return (0 <= x) & (x < 1)
+
+    def eval(self, x, deriv=False):
+        assert deriv == False
+        l, n = self.labda
+        return 1.0 * self.eval_mother(2**l * x - n)
+
+class BaseWavelet:
+    def __init__(self, labda, parents, single_scale):
+        self.labda = labda
+        self.parents = parents
+        self.single_scale = list(single_scale)
+        self.children = []
+        self.support = []
+        for phi, coeff in self.single_scale:
+            self.support.extend(phi.support)
+            # Register this wavelet in the corresponding phi.
+            phi.multi_scale.append((self, coeff))
+
+    def eval(self, x, deriv=False):
+        result = 0
+        for phi, coeff_ss in self.single_scale:
+            result += coeff_ss * phi.eval(x, deriv)
+        return result
+
+    def __repr__(self):
+        return "{}({}, {})".format(self.__class__.__name__, *self.labda)
+
+class HaarWavelet(BaseWavelet):
+    def __init__(self, labda, parents, single_scale):
+        super().__init__(labda, parents, single_scale)
+
+    def refine(self):
+        if self.children: return
+        phi_left, phi_right = [phi for phi, _ in self.single_scale]
+        phi_left.refine()
+        phi_right.refine()
+
+        l, n = self.labda
+        child_left = HaarWavelet((l+1, 2*n), self, [(phi_left.children[0], 1), (phi_left.children[1], -1)])
+        child_right = HaarWavelet((l+1, 2*n+1), self, [(phi_right.children[0], 1), (phi_right.children[1], -1)])
+        self.children = [child_left, child_right]
+
+class BaseBasis:
+    @classmethod
+    def uniform_basis(cls, max_level):
+        assert max_level >= 1
+        basis = cls()
+
+        # Start refining all wavelets
+        Lambda = [] + basis.mother_wavelets
+        Lambda_l = Lambda
+        for _ in range(max_level - 1):
+            Lambda_new = []
+            for psi in Lambda_l:
+                psi.refine()
+                Lambda_new.extend(psi.children)
+            Lambda_l = Lambda_new
+            Lambda.extend(Lambda_l)
+
+        Delta = basis.mother_scalings + list({phi for psi in Lambda for phi, _ in psi.single_scale})
+        Lambda = basis.mother_scalings + Lambda
+        return basis, MultiscaleIndices(Lambda), MultiscaleIndices(Delta)
+
+    @classmethod
+    def origin_refined_basis(cls, max_level):
+        assert max_level >= 1
+        basis = cls()
+
+        n_wavelets = len(basis.mother_wavelets)
+        assert n_wavelets == 1
+
+        Lambda = [] + basis.mother_wavelets
+        Lambda_l = Lambda
+        for _ in range(max_level - 1):
+            parent = Lambda[-1]
+            parent.refine()
+            Lambda.append(parent.children[0])
+
+        Delta = basis.mother_scalings + list({phi for psi in Lambda for phi, _ in psi.single_scale})
+        Lambda = basis.mother_scalings + Lambda
+        return basis, MultiscaleIndices(Lambda), MultiscaleIndices(Delta)
+
+    @property
+    def P(self):
+        def row(phi):
+            return {phi : coeff for phi, coeff in phi.restrict()}
+
+        def col(phi):
+            return {phi : coeff for phi, coeff in phi.prolongate()}
+
+        return LinearOperator(row, col)
+
+    @property
+    def Q(self):
+        def row(phi):
+            return {psi : coeff for psi, coeff in phi.multi_scale}
+
+        def col(psi):
+            return {phi : coeff for phi, coeff in psi.single_scale}
+
+        return LinearOperator(row, col)
+
+    @staticmethod
+    def scaling_mass():
+        def row(phi):
+            return {phi : coeff for phi, coeff in phi.mass()}
+        return LinearOperator(row)
+
+class HaarBasis(BaseBasis):
+    # Create static mother scaling function.
+    mother_scaling = HaarScaling((0, 0), None, [Element.mother_element])
+    mother_scalings = [mother_scaling]
+    mother_scaling.refine()
+
+    # Create static mother wavelet function.
+    mother_wavelet = HaarWavelet((1, 0), None, [(mother_scaling.children[0], 1), (mother_scaling.children[1], -1)])
+    mother_wavelets = [mother_wavelet]
+
+    def __init__(self):
+        pass
+
+class ThreePointScaling(BaseScaling):
+    def __init__(self, labda, parents, support):
+        super().__init__(labda, parents, support)
+        self.nbr_left = None
+        self.nbr_right = None
+
+        self.child_left = None
+        self.child_mid  = None
+        self.child_right= None
+    def refine_mid(self):
+        if self.child_mid: return self.child_mid
+        l, n = self.labda
+
+        # Calculate the support of the refined hat.
+        for elem in self.support: elem.bisect()
+        child_support = []
+        if n > 0: child_support.append(self.support[0].children[1])
+        if n < 2**l: child_support.append(self.support[-1].children[0])
+
+        # Create element.
+        child = ThreePointScaling((l+1, 2*n), [self], child_support)
+        self.child_mid = child
+
+        # Update nbrs.
+        if self.child_left:
+            self.child_left.nbr_right = child
+            child.nbr_left = self.child_left
+
+        if self.child_right:
+            self.child_right.nbr_left = child
+            child.nbr_right = self.child_right
+
+        return child
+
+    def refine_left(self):
+        """ This refines the hat function on the left. """
+        assert self.nbr_left
+        if self.child_left:
+            assert self.nbr_left.child_right == self.child_left
+            return self.child_left
+
+        # Some shortcuts for ease of reading.
+        l, n = self.labda
+        phi_left  = self.nbr_left
+        phi_right = self
+
+        # Create child element.
+        child = ThreePointScaling((l+1, n*2-1), [phi_left, phi_right], phi_right.support[0].children)
+        phi_left.child_right = child
+        phi_right.child_left = child
+
+        # Update nbrs.
+        if phi_left.child_mid:
+            phi_left.child_mid.nbr_right = child
+            child.nbr_left = phi_left.child_mid
+
+        if phi_right.child_mid:
+            phi_right.child_mid.nbr_left = child
+            child.nbr_right = phi_right.child_mid
+
+        return child
+
+    def refine_right(self):
+        assert self.nbr_right
+        return self.nbr_right.refine_left()
+
+    def prolongate(self):
+        l, n = self.labda
+        result = [(self.refine_mid(), 1)]
+        if n > 0: result.append((self.refine_left(), 0.5))
+        if n < 2**l: result.append((self.refine_right(), 0.5))
+        return result
+
+    def restrict(self):
+        if len(self.parents) == 1:
+            return [(self.parents[0], 1)]
+        else:
+            return [(parent, 0.5) for parent in self.parents]
+
+    def mass(self):
+        result = []
+        l, n = self.labda
+        self_ip = 0
+        if n > 0:
+            result.append((self.nbr_left, 1/6 * 2**-l))
+            self_ip += 1/3 * 2**-l
+        if n < 2**l:
+            result.append((self.nbr_right, 1/6 * 2**-l))
+            self_ip += 1/3 * 2**-l
+
+        result.append((self, self_ip))
+        return result
+
+    @staticmethod
+    def eval_mother(x, deriv=False):
+        """ Evaluates the hat function on [-1,1] centered at 0. """
+        left_mask = (-1 < x) & (x <= 0)
+        right_mask = (0 < x) & (x < 1)
+
+        if not deriv:
+            return left_mask * (1+x) + right_mask * (1-x)
+        else:
+            return left_mask * 1 + right_mask * -1
+
+    def eval(self, x, deriv=False):
+        l, n = self.labda
+        c = 2**l if deriv else 1.0
+        return c * self.eval_mother(2**l * x - n, deriv)
+
+class ThreePointWavelet(BaseWavelet):
+    def __init__(self, labda, parents, single_scale):
+        super().__init__(labda, parents, single_scale)
+
+    def refine(self):
+        if self.children: return
+        phi_left, phi_mid, phi_right = [phi for phi, _ in self.single_scale]
+        l, n = self.labda
+        scaling = 2**((l+1)/2)
+
+        # First refine the left part
+        phi_children = phi_left.refine_mid(), phi_mid.refine_left(), phi_mid.refine_mid()
+        if n == 0:
+            child_left = ThreePointWavelet((l+1, 2*n), self, zip(phi_children, (-1*scaling, 1*scaling, -1/2*scaling)))
+        else:
+            child_left = ThreePointWavelet((l+1, 2*n), self, zip(phi_children, (-1/2*scaling, 1*scaling, -1/2*scaling)))
+
+        # Now refine the right part
+        phi_children = phi_mid.refine_mid(), phi_mid.refine_right(), phi_right.refine_mid()
+        if  n == 2**(l-1) - 1:
+            child_right = ThreePointWavelet((l+1, 2*n+1), self, zip(phi_children, (-1/2* scaling, 1 * scaling, -1 * scaling)))
+        else:
+            child_right = ThreePointWavelet((l+1, 2*n+1), self, zip(phi_children, (-1/2* scaling, 1 * scaling, -1/2 * scaling)))
+
+        self.children = [child_left, child_right]
+
+class ThreePointBasis(BaseBasis):
+    # Create static mother scaling functions.
+    mother_scalings = [ThreePointScaling((0, 0), None, [Element.mother_element]),ThreePointScaling((0, 1), None, [Element.mother_element])]
+    mother_scalings[0].nbr_right = mother_scalings[1]
+    mother_scalings[1].nbr_left = mother_scalings[0]
+
+    # Create the static mother wavelet.
+    mother_wavelets = [ThreePointWavelet((1, 0), None, [(mother_scalings[0].refine_mid(), -2**(1/2)), (mother_scalings[0].refine_right(), 2**(1/2)), (mother_scalings[1].refine_mid(), -2**(1/2))])]
+
+    def __init__(self):
+        pass
