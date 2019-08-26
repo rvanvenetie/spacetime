cb59fa082813fda7a840769624f6c87015e47ac0
Raymond van Venetie <rayvanve@gmail.com>
2019-08-26 16:39:33 +0200

    craeted new test for lin combination

--- Python/time/applicator_tree_test.py
+++ Python/time/applicator_tree_test.py
@@ -1,13 +1,17 @@
-from applicator_tree import Applicator
-from basis_tree import support_to_interval
-from basis_tree import HaarBasis, ThreePointBasis, OrthoBasis
-from indexed_vector import IndexedVector
+from fractions import Fraction
 
 import numpy as np
+from pytest import approx
 from scipy.integrate import quad
+
+from applicator_tree import Applicator
+from basis_tree import (HaarBasis, OrthoBasis, ThreePointBasis,
+                        support_to_interval)
+from indexed_vector import IndexedVector
 
 np.random.seed(0)
 np.set_printoptions(linewidth=10000, precision=3)
+
 
 def test_haar_multiscale_mass():
     """ Test that the multiscale Haar mass matrix is indeed diagonal. """
@@ -22,9 +26,10 @@ def test_haar_multiscale_mass():
                 np_vec = np.random.rand(len(Lambda_l))
                 vec = IndexedVector(Lambda_l, np_vec)
                 res = applicator.apply(vec)
-                assert np.allclose(
-                    [(1.0 if phi.labda[0] == 0 else 2**(phi.labda[0] - 1)) * res[phi]
-                     for phi in Lambda_l], np_vec)
+                assert np.allclose([(1.0 if phi.labda[0] == 0 else 2**
+                                     (phi.labda[0] - 1)) * res[phi]
+                                    for phi in Lambda_l], np_vec)
+
 
 def test_orthonormal_multiscale_mass():
     """ Test that the multiscale mass operator is the identity. """
@@ -38,7 +43,7 @@ def test_orthonormal_multiscale_mass():
             eye = np.eye(len(Lambda_l))
             res_matrix = np.zeros([len(Lambda_l), len(Lambda_l)])
             for i, psi in enumerate(Lambda_l):
-                vec = IndexedVector({psi : 1.0})
+                vec = IndexedVector({psi: 1.0})
                 res = applicator.apply(vec)
                 print(psi, res)
                 res_matrix[:, i] = res.asarray(Lambda_l)
@@ -64,16 +69,19 @@ def test_multiscale_operator_quadrature():
         (hbu, hbu, HaarBasis.scaling_mass(), (False, False)),
         (hbo, hbo, HaarBasis.scaling_mass(), (False, False)),
         (hbu, hbo, HaarBasis.scaling_mass(), (False, False)),
-        (oru, oru, HaarBasis.scaling_mass(), (False, False)),
-        (oro, oro, HaarBasis.scaling_mass(), (False, False)),
-        (oru, oro, HaarBasis.scaling_mass(), (False, False)),
+        (oru, oru, OrthoBasis.scaling_mass(), (False, False)),
+        (oro, oro, OrthoBasis.scaling_mass(), (False, False)),
+        (oru, oro, OrthoBasis.scaling_mass(), (False, False)),
         (tpu, tpu, ThreePointBasis.scaling_mass(), (False, False)),
         (tpo, tpo, ThreePointBasis.scaling_mass(), (False, False)),
+        (tpu, tpo, ThreePointBasis.scaling_mass(), (False, False)),
     ]:
         basis_in, Lambda_in, Delta_in = basis_in
         basis_out, Lambda_out, Delta_out = basis_out
-        print('Calculating results for: basis_in={}\tbasis_out={}'.format(basis_in.__class__.__name__, basis_out.__class__.__name__))
-        applicator = Applicator(basis_in, operator, Lambda_in, basis_out, Lambda_out)
+        print('Calculating results for: basis_in={}\tbasis_out={}'.format(
+            basis_in.__class__.__name__, basis_out.__class__.__name__))
+        applicator = Applicator(basis_in, operator, Lambda_in, basis_out,
+                                Lambda_out)
         eye = np.eye(len(Lambda_in))
         resmat = np.zeros([len(Lambda_in), len(Lambda_out)])
         truemat = np.zeros([len(Lambda_in), len(Lambda_out)])
@@ -106,3 +114,61 @@ def test_multiscale_operator_quadrature():
             print(np.round(resmat, decimals=3))
             print(np.round(truemat, decimals=3))
             raise
+
+
+def test_multiscale_operator_quadrature_lin_comb():
+    """ Test that the multiscale matrix equals that found with quadrature. """
+    uml = 4
+    oml = 5
+    hbu = HaarBasis.uniform_basis(max_level=uml)
+    hbo = HaarBasis.origin_refined_basis(max_level=oml)
+    oru = OrthoBasis.uniform_basis(max_level=uml)
+    oro = OrthoBasis.origin_refined_basis(max_level=oml)
+    tpu = ThreePointBasis.uniform_basis(max_level=uml)
+    tpo = ThreePointBasis.origin_refined_basis(max_level=oml)
+    for basis_in, basis_out, operator, deriv in [
+        (hbu, hbu, HaarBasis.scaling_mass(), (False, False)),
+        (hbo, hbo, HaarBasis.scaling_mass(), (False, False)),
+        (hbu, hbo, HaarBasis.scaling_mass(), (False, False)),
+        (oru, oru, OrthoBasis.scaling_mass(), (False, False)),
+        (oro, oro, OrthoBasis.scaling_mass(), (False, False)),
+        (oru, oro, OrthoBasis.scaling_mass(), (False, False)),
+        (tpu, tpu, ThreePointBasis.scaling_mass(), (False, False)),
+        (tpo, tpo, ThreePointBasis.scaling_mass(), (False, False)),
+        (tpu, tpo, ThreePointBasis.scaling_mass(), (False, False)),
+    ]:
+        basis_in, Lambda_in, _ = basis_in
+        basis_out, Lambda_out, _ = basis_out
+        print('Calculating results for: basis_in={}\tbasis_out={}'.format(
+            basis_in.__class__.__name__, basis_out.__class__.__name__))
+        applicator = Applicator(basis_in, operator, Lambda_in, basis_out,
+                                Lambda_out)
+        for _ in range(3):
+            vec_in = IndexedVector(Lambda_in, np.random.rand(len(Lambda_in)))
+            vec_out = applicator.apply(vec_in)
+
+            # Define function that evaluates lin. comb. Psi_Lambda_in
+            def psi_vec_in_eval(x, deriv):
+                result = 0
+                for psi_in, val_in in vec_in.items():
+                    result += val_in * psi_in.eval(x, deriv)
+                return result
+
+            for psi_out in Lambda_out:
+                supp_psi_out = support_to_interval(psi_out.support)
+
+                # Calculate all the breakpoints in this interval.
+                points = [supp_psi_out.a]
+                h = Fraction(
+                    1, 2**max(Lambda_in.maximum_level,
+                              Lambda_out.maximum_level))
+                while points[-1] < supp_psi_out.b:
+                    points.append(points[-1] + h)
+                points = list(map(float, points))
+
+                # Apply quadrature.
+                true_val = quad(lambda x: psi_vec_in_eval(x, deriv=deriv[0]) * psi_out.eval(x, deriv=deriv[1]),
+                                supp_psi_out.a,
+                                supp_psi_out.b, points=points)[0]
+
+                assert true_val == approx(vec_out[psi_out])
