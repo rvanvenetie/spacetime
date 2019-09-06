81a07946b3161b5de05f5bfe3ce1cdf3d1354f12
Jan Westerdiep <janner@gmail.com>
2019-09-06 15:36:21 +0200

    manually reverting changes made to branch spacetime by my git fuckup

--- Python/spacetime/applicator.py
+++ Python/spacetime/applicator.py
@@ -1,13 +1,10 @@
-from tree import *
-
-
 class Applicator:
     """ Class that implements a tensor product operator. """
     def __init__(self,
                  basis_in,
                  Lambda_in,
-                 applicator_time,
-                 applicator_space,
+                 operator_time,
+                 operator_space,
                  basis_out=None,
                  Lambda_out=None):
         """ Initialize the applicator.
@@ -15,101 +12,56 @@ class Applicator:
         Arguments:
             basis_in: A tensor-basis input.
             Lambda_in: A double tree index set input corresponding to the input.
-            applicator_time: The applicator to be applied to the time axis.
-            applicator_space: The applicator to be applied on the space axis.
+            operator_time: The operator to be applied to the time axis.
+            operator_space: The operator to be applied on the space axis.
             basis_out: A tensor-basis output.
             Lambda_out: The double tree index set corresponding to the output.
         """
         self.basis_in = basis_in
         self.Lambda_in = Lambda_in
-        self.applicator_time = applicator_time
-        self.applicator_space = applicator_space
+        self.operator_time = operator_time
+        self.operator_space = operator_space
 
-        self.basis_out = basis_out if basis_out else basis_in
-        self.Lambda_out = Lambda_out if Lambda_out else Lambda_in
-
-        if self.Lambda_out.root.nodes != self.Lambda_in.root.nodes:
-            raise NotImplementedError("This needs a difficult coupling!")
-
-        # Reset element.psi_out field in preparation for Sigma.
-        for psi_out in self.Lambda_out.root.bfs(0):
-            for elem in psi_out.nodes[0].support:
-                elem.psi_out = []
-        for psi_out in self.Lambda_out.root.bfs(0):
-            for elem in psi_out.nodes[0].support:
-                elem.psi_out.append(psi_out.nodes[0])
+        if basis_out is None:
+            basis_out = basis_in
+            Lambda_out = Lambda_in
+        self.basis_out = basis_out
+        self.Lambda_out = Lambda_out
 
     def sigma(self):
-        """ Constructs the double tree Sigma for Lambda_in and Lambda_out. """
-        sigma_root = self.Lambda_in.root.__class__(
-            (self.Lambda_in.root.nodes[0], self.Lambda_out.root.nodes[1]))
-
-        # Copy self.Lambda_in.project(0) into self.sigma and traverse.
-        sigma_root.union_from(self.Lambda_in.project(0), i=0)
-        empty_labdas = []
-        for psi_in_labda in sigma_root.bfs(0):
+        """ Returns the double tree Sigma for Lambda_in and Lambda_out. """
+        result = {}
+        for psi_in_labda in self.Lambda_in.project(1):
             # Get support of psi_in_labda on level + 1.
             children = [
-                child for elem in psi_in_labda.nodes[0].support
+                child for elem in psi_in_labda.support
                 for child in elem.children
             ]
 
-            # Collect all fiber(1, mu) for psi_out_mu that intersect with
-            # support of psi_in_labda, and put their union into sigma.
-            labda_empty = True
-            for child in children:
-                for mu in child.psi_out:
-                    labda_empty = False
-                    psi_in_labda.union_from(self.Lambda_out.fiber(1, mu), 1)
-            if labda_empty:
-                empty_labdas.append(psi_in_labda)
+            # Collect all fiber(2, mu) for psi_out_mu that
+            # intersect with support of psi_in_labda.
+            space_out = set([
+                self.Lambda_out.fiber(2, mu) for child in children
+                for mu in child.psi_out
+            ])
 
-        # Zucht hier ben ik niet zo blij mee.
-        for psi_in_labda in reversed(empty_labdas):
-            for parent in psi_in_labda.nodes[0].parents:
-                assert parent.is_full()
-        for psi_in_labda in reversed(empty_labdas):
-            psi_in_labda.coarsen()
-        return DoubleTree(sigma_root)
-
-    def theta(self):
-        theta = DoubleTree(
-            (self.Lambda_in.root.nodes[0], self.Lambda_out.root.nodes[1]))
-        sigma.root.union_from(self.Lambda_out.root, i=1)
-        for psi_out_labda in theta.root.bfs(1):
-            # phew...
-            pass
+            result[psi_in_labda] = space_out
+        return result
 
     def apply(self, vec):
-        """ Apply the tensor product applicator to the given vector. """
+        """ Apply the tensor product operator to the given vector. """
 
-        # Calculate R_sigma(Id x A_1)I_Lambda
-        v = {}
         sigma = self.sigma()
-        for psi_in_labda in sigma.project(0):
-            fiber_in = self.Lambda_in.fiber(1, psi_in_labda)
-            fiber_out = sigma.fiber(1, psi_in_labda)
-            v += self.applicator_space.apply(vec, fiber_in, fiber_out)
-
-        # Calculate R_Lambda(L_0 x Id)I_Sigma
-        w = {}
-        for psi_out_labda in self.Lambda_out.project(1):
-            fiber_in = sigma.fiber(0, psi_out_labda)
-            fiber_out = self.Lambda_out.fiber(0, psi_out_labda)
-            w += self.applicator_time.apply_low(v, fiber_in, fiber_out)
-
-        # Calculate R_Theta(U_1 x Id)I_Lambda
-        v = {}
         theta = self.theta()
-        for psi_out_labda in theta.project(1):
-            fiber_in = self.Lambda_in.fiber(0, psi_out_labda)
-            fiber_out = theta.fiber(0, psi_out_labda)
-            v += self.applicator_time.apply_upp(vec, fiber_in, fiber_out)
 
-        # Calculate R_Lambda(id x A2)I_Theta
-        for psi_out_labda in self.Lambda_out.project(0):
-            fiber_in = theta.fiber(1, psi_out_labda)
-            fiber_out = self.Lambda_out.fiber(1, psi_out_labda)
-            w += self.applicator_space.apply(v, fiber_in, fiber_out)
+        # Calculate R_sigma(Id x A_2)I_Lambda
+        proj_time_sigma = sigma.project(1)
 
-        return w
+        v = {}
+        for psi_in_labda in proj_time_sigma:
+            labda_fiber_in = self.Lambda_in.fiber(2, psi_in_labda)
+            labda_fiber_out = self.sigma.fiber(2, psi_in_labda)
+            v += self.operator_space.apply(vec, labda_fiber_in,
+                                           labda_fiber_out)
+
+        # Now
