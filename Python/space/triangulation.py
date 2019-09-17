d2ce7c6fe439e9c3e47202fdd7ef481b5bd40816
Raymond van Venetie <rayvanve@gmail.com>
2019-09-17 15:15:21 +0200

    created vertex tree

--- Python/space/triangulation.py
+++ Python/space/triangulation.py
@@ -6,20 +6,46 @@ import numpy as np
 
 
 class Vertex:
-    """ A vertex in a locally refined triangulation. """
-    def __init__(self, idx, x, y, on_domain_boundary):
-        self.idx = idx
+    """ A vertex in a locally refined triangulation.
+    
+    Vertices also form a (family)tree, induces by the NVB-relation.
+    """
+    def __init__(self,
+                 labda,
+                 x,
+                 y,
+                 on_domain_boundary,
+                 parents=None,
+                 children=None):
+        self.labda = labda
         self.x = x
         self.y = y
         self.on_domain_boundary = on_domain_boundary
 
+        self.parents = parents if parents else []
+        self.children = children if children else []
+
+        # Sanity check.
+        assert (all([p.level == self.level - 1 for p in self.parents]))
+
+    @property
+    def level(self):
+        return self.labda[0]
+
+    @property
+    def idx(self):
+        return self.labda[1]
+
     def as_array(self):
         return np.array([self.x, self.y], dtype=float)
+
+    def __repr__(self):
+        return '{}'.format(self.labda)
 
 
 class Element:
     """ A element as part of a locally refined triangulation. """
-    def __init__(self, idx, vertices, parent=None):
+    def __init__(self, labda, vertices, parent=None):
         """ Instantiates the element object.
 
         Arguments:
@@ -27,7 +53,7 @@ class Element:
             vertices: array of three Vertex references. 
             parent: reference to the parent of this element.
             """
-        self.idx = idx
+        self.labda = labda
         self.vertices = vertices
         self.parent = parent
         self.children = []  # References to the children of this element.
@@ -37,7 +63,7 @@ class Element:
         if self.parent:
             self.area = self.parent.area / 2
 
-    def newest_vertex_id(self):
+    def newest_vertex(self):
         """ Returns the newest vertex, i.e., vertex 0. """
         return self.vertices[0]
 
@@ -52,6 +78,17 @@ class Element:
     def is_leaf(self):
         return not len(self.children)
 
+    @property
+    def level(self):
+        return self.labda[0]
+
+    @property
+    def idx(self):
+        return self.labda[1]
+
+    def __repr__(self):
+        return '{}: {}'.format(self.labda, self.vertices)
+
 
 class Triangulation:
     def __init__(self, vertices, elements):
@@ -62,14 +99,15 @@ class Triangulation:
             elements: Tx3 matrix of integers: the indices inside the vertices array.
         """
         self.vertices = [
-            Vertex(idx, *vert, on_domain_boundary=False)
+            Vertex((0, idx), *vert, on_domain_boundary=False)
             for idx, vert in enumerate(vertices)
         ]
         self.elements = [
-            Element(i, [self.vertices[idx] for idx in elem])
+            Element((0, i), [self.vertices[idx] for idx in elem])
             for (i, elem) in enumerate(elements)
         ]
-        self.root_ids = set([elem.idx for elem in self.elements])
+        self.element_roots = self.elements.copy()
+        self.vertex_roots = self.vertices.copy()
         self.history = []
 
         for elem in self.elements:
@@ -99,7 +137,29 @@ class Triangulation:
         elements = [[0, 2, 3], [1, 3, 2]]
         return Triangulation(vertices, elements)
 
-    def _bisect(self, elem, new_vertex=False):
+    def _create_new_vertex(self, elem1, elem2=None):
+        """ Creates a new vertex necessary for bisection.
+
+        Args:
+            elem1: The element which will be bisected.
+            elem2: If elem1.refinement_edge() is not on the boundary, this
+                   should be the element on the other side. 
+        """
+        parents = [elem1.newest_vertex()]
+        if elem2: parents.append(elem2.newest_vertex())
+        godparents = elem1.vertices[1:]
+        new_vertex = Vertex((parents[0].level + 1, len(self.vertices)),
+                            (godparents[0].x + godparents[1].x) / 2,
+                            (godparents[0].y + godparents[1].y) / 2,
+                            on_domain_boundary=elem2 is None,
+                            parents=parents)
+        for parent in parents:
+            parent.children.append(new_vertex)
+        self.vertices.append(new_vertex)
+        self.history.append((new_vertex.idx, elem1.idx))
+        return new_vertex
+
+    def _bisect(self, elem, new_vertex):
         """ Bisects a given element using Newest Vertex Bisection.
 
         Arguments:
@@ -107,23 +167,13 @@ class Triangulation:
             new_vertex_id: index of the new vertex in the `self.vertices` array. If
                 not passed, creates a new vertex.
         """
-        vertices = elem.vertices
-        if not new_vertex:
-            # Create the new vertex object.
-            parents = vertices[1:]
-            on_domain_boundary = all([p.on_domain_boundary for p in parents])
-            new_vertex_id = len(self.vertices)
-            new_vertex = Vertex(new_vertex_id,
-                                (parents[0].x + parents[1].x) / 2,
-                                (parents[0].y + parents[1].y) / 2,
-                                on_domain_boundary)
-            self.vertices.append(new_vertex)
-            self.history.append((new_vertex_id, elem.idx))
         child1_id = len(self.elements)
         child2_id = len(self.elements) + 1
-        child1 = Element(child1_id, [new_vertex, vertices[0], vertices[1]],
+        child1 = Element((elem.level + 1, child1_id),
+                         [new_vertex, elem.vertices[0], elem.vertices[1]],
                          parent=elem)
-        child2 = Element(child2_id, [new_vertex, vertices[2], vertices[0]],
+        child2 = Element((elem.level + 1, child2_id),
+                         [new_vertex, elem.vertices[2], elem.vertices[0]],
                          parent=elem)
 
         self.elements.extend([child1, child2])
@@ -146,16 +196,16 @@ class Triangulation:
     def _bisect_pair(self, elem1, elem2):
         """ Bisects a pair of elemangles. Sets the childrens neighbours. """
         assert elem1.edge(0) == elem2.reversed_edge(0)
-        child11, child12 = self._bisect(
-            elem1)  # child11s newest vertex is new.
-        child21, child22 = self._bisect(elem2,
-                                        new_vertex=child11.newest_vertex_id())
+
+        new_vertex = self._create_new_vertex(elem1, elem2)
+        child11, child12 = self._bisect(elem1, new_vertex)
+        child21, child22 = self._bisect(elem2, new_vertex)
+
         # Set the neighbour info along shared edges of the children.
         child11.neighbours[1] = child22
         child12.neighbours[2] = child21
         child21.neighbours[1] = child12
         child22.neighbours[2] = child11
-        child11.newest_vertex_id().on_domain_boundary = False
 
     def refine(self, elem):
         """ Refines the trianglulation so that element `elem` is bisected.
@@ -166,9 +216,8 @@ class Triangulation:
             return
         nbr = elem.neighbours[0]
         if not nbr:  # Refinement edge of `elem` is on domain boundary.
-            self._bisect(elem)
-            return
-        if nbr.edge(0) == elem.reversed_edge(0):  # Shared refinement edge.
+            self._bisect(elem, self._create_new_vertex(elem))
+        elif nbr.edge(0) == elem.reversed_edge(0):  # Shared refinement edge.
             self._bisect_pair(elem, nbr)
         else:
             self.refine(nbr)
