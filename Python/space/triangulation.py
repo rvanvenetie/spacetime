72d71c9b4bb3289680663169d6ce3bfaa78d0a2e
Raymond van Venetie <rayvanve@gmail.com>
2019-09-19 11:08:16 +0200

    binary node specification

--- Python/space/triangulation.py
+++ Python/space/triangulation.py
@@ -2,8 +2,10 @@ import itertools
 
 import numpy as np
 
+from datastructures.tree import NodeAbstract
 
-class Vertex:
+
+class Vertex(NodeAbstract):
     """ A vertex in a locally refined triangulation.
     
     Vertices also form a (family)tree, induced by the NVB-relation.
@@ -24,17 +26,22 @@ class Vertex:
                  patch=None,
                  parents=None,
                  children=None):
+        super().__init__(parents=parents, children=children)
         self.labda = labda
         self.x = x
         self.y = y
         self.on_domain_boundary = on_domain_boundary
 
         self.patch = patch if patch else []
-        self.parents = parents if parents else []
-        self.children = children if children else []
 
         # Sanity check.
         assert (all([p.level == self.level - 1 for p in self.parents]))
+
+    def refine(self):
+        raise TypeError('It is not possible to refine a vertex.')
+
+    def is_full(self):
+        return not self.children or len(self.children) == len(self.patch)
 
     @property
     def level(self):
@@ -51,7 +58,7 @@ class Vertex:
         return '{}'.format(self.labda)
 
 
-class Element:
+class Element(NodeAbstract):
     """ A element as part of a locally refined triangulation. """
     def __init__(self, labda, vertices, parent=None):
         """ Instantiates the element object.
@@ -61,15 +68,31 @@ class Element:
             vertices: array of three Vertex references. 
             parent: reference to the parent of this element.
             """
+        super().__init__(parents=[parent])
         self.labda = labda
         self.vertices = vertices
-        self.parent = parent
         self.children = []  # References to the children of this element.
         # Indices of my neighbours, ordered by the edge opposite vertex i.
         self.neighbours = [None, None, None]
 
-        if self.parent:
-            self.area = self.parent.area / 2
+        if parent:
+            self.area = parent.area / 2
+
+    @property
+    def parent(self):
+        return self.parents[0]
+
+    @parent.setter
+    def parent(self, parent):
+        assert isinstance(parent, Element)
+        self.parents = [parent]
+
+    def refine(self):
+        raise TypeError(
+            'It is not yet possible to directly refine an element.')
+
+    def is_full(self):
+        return len(self.elements) in [0, 2]
 
     def newest_vertex(self):
         """ Returns the newest vertex, i.e., vertex 0. """
