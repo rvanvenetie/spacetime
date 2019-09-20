93beb6616b4fb7a1faa1e37eb9530c5a5686c169
Jan Westerdiep <janner@gmail.com>
2019-09-20 11:26:14 +0200

    added test

--- Python/space/basis_test.py
+++ Python/space/basis_test.py
@@ -12,6 +12,32 @@ def test_hierarchical_basis():
     assert len(T.elements) == 2
 
 
+def test_diamond_no_overrefine():
+    T = Triangulation.unit_square()
+    for _ in range(2):
+        T.refine_uniform()
+    # Find the first leaf and refine it, creating a diamond in the vertex tree.
+    leaves = [elem for elem in T.elements if elem.is_leaf()]
+    assert len(leaves) == 8
+    T.refine(leaves[0])
+
+    basis_meta_root = HierarchicalBasisFunction.from_triangulation(T)
+
+    # Refine and find the node view with the single vertex on level 1.
+    level1_fn = basis_meta_root.roots[0].refine()[0]
+    assert level1_fn.level == 1
+
+    # Refine *a single child* and find the node view on level 2.
+    level2_fn = level1_fn.refine(children=[level1_fn.node.children[0]])[0]
+    assert level2_fn.level == 2
+
+    # Refine this new node view; the diamond should be refined but we should
+    # *not* have the entire vertex tree.
+    level2_fn.refine()
+    assert len(basis_meta_root.bfs()) == 8
+    assert len(T.vertices) == 10
+
+
 def test_refine_hierarchical_basis():
     T = Triangulation.unit_square()
     basis_meta_root = HierarchicalBasisFunction.from_triangulation(T)
