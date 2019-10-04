1a2aaf10c234fc724c2c67884b7f8fc6cc388128
Raymond van Venetie <rayvanve@gmail.com>
2019-10-04 17:52:42 +0200

    added stiff quadrature test

--- Python/space/basis.py
+++ Python/space/basis.py
@@ -1,3 +1,5 @@
+import numpy as np
+
 from ..datastructures.function import FunctionInterface
 from ..datastructures.tree import MetaRoot
 from ..datastructures.tree_view import MetaRootView, NodeView
@@ -20,13 +22,20 @@ class HierarchicalBasisFunction(FunctionInterface, NodeView):
 
     def eval(self, x, deriv=False):
         assert len(x) == 2
-        assert not deriv
         for elem in self.support:
             bary = elem.to_barycentric_coordinates(x)
             # Check if this triangle contains the point.
             if all(bary >= 0) and bary[0] + bary[1] <= 1:
-                return bary[elem.vertices.index(self.node)]
-        return 0
+                v_index = elem.vertices.index(self.node)
+                if not deriv: return bary[v_index]
+                else:
+                    edge_opposite = elem.vertices[v_index - 1].as_array(
+                    ) - elem.vertices[(v_index + 1) % 3].as_array()
+                    normal = np.array([-edge_opposite[1], edge_opposite[0]])
+                    return -normal / (2 * elem.area)
+
+        if not deriv: return 0
+        else: return np.zeros(2)
 
     @staticmethod
     def from_triangulation(triangulation):
