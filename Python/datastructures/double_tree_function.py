9c406cbda472d65c94cf658ca431ac0cbda1ff05
Jan Westerdiep <janner@gmail.com>
2019-10-16 17:25:32 +0200

    bla

--- Python/datastructures/double_tree_function.py
+++ Python/datastructures/double_tree_function.py
@@ -1,4 +1,6 @@
-from ..datastructures.double_tree_vector import DoubleTreeVector
+from ..datastructures.double_tree_vector import (DoubleNodeVector,
+                                                 DoubleTreeVector,
+                                                 FrozenDoubleNodeVector)
 
 
 class DoubleTreeFunction(DoubleTreeVector):
@@ -18,14 +20,25 @@ class DoubleTreeFunction(DoubleTreeVector):
         return result
 
     @staticmethod
-    def project_function(self, dbl_tree, g, g_order):
+    def interpolant_of(dbl_tree, g, g_order):
+        """ Interpolate `g` onto the span of `dbl_tree` using quadrature.
+
+        Uses quadrature to find
+            Interp(g) := sum_{lambda in dbl_tree} <g, phi_lambda> phi_lambda.
+
+        Arguments:
+            dbl_tree: the double tree to compute the projection on.
+            g: a sum of separable functions: g == sum_i g_0i(t) * g_1i(x).
+            g_order: tuple; maximum order of `g` in either axis.
+        """
         def call_quad(nv, _):
             """ Helper function to do the quadrature for the input function. """
             if nv.is_metaroot(): return
-            nv.value = sum(nv.nodes[0].inner_quad(fn0, fn_order=fn_order[0]) *
-                           nv.nodes[1].inner_quad(fn1, fn_order=g_order[1])
+            nv.value = sum(nv.nodes[0].inner_quad(g0, g_order=g_order[0]) *
+                           nv.nodes[1].inner_quad(g1, g_order=g_order[1])
                            for g0, g1 in g)
 
         dbl_tree_fn = dbl_tree.deep_copy(mlt_node_cls=DoubleNodeVector,
                                          mlt_tree_cls=DoubleTreeFunction,
                                          call_postprocess=call_quad)
+        return dbl_tree_fn
