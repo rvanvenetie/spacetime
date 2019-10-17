df4a5a5e9d7fd7fb47fe6ede3c211583163d7e4a
Jan Westerdiep <janner@gmail.com>
2019-10-17 10:58:30 +0200

    finally

--- Python/datastructures/multi_tree_function.py
+++ Python/datastructures/multi_tree_function.py
@@ -1,6 +1,7 @@
 import numpy as np
 
 from ..datastructures.multi_tree_vector import MultiTreeVector
+from ..datastructures.double_tree_vector import DoubleTreeVector
 
 
 class MultiTreeFunction(MultiTreeVector):
@@ -8,16 +9,26 @@ class MultiTreeFunction(MultiTreeVector):
     def __init__(self, root):
         super().__init__(root=root)
 
-    def eval(self, args, deriv=(False, False)):
-        """ Evaluate in a stupid way. """
-        assert x.shape[0] == 2
-        if isinstance(t, np.ndarray):
-            assert t.shape[0] == x.shape[1]
-            result = np.zeros(t.shape) if not deriv[1] else np.zeros(x.shape)
+    def eval(self, coords):
+        """ Evaluate in a stupid way.
+        
+        Arguments:
+            coords: a list of coords, each size (dim, N)."""
+        assert len(coords) == self.root.dim
+        # If the input coords are arrays, return an array, else a number.
+        if any(isinstance(coord, np.ndarray) for coord in coords):
+            assert all(coords[0].shape[-1] == coord.shape[-1]
+                       for coord in coords)
+            result = np.zeros(coords[0].shape[-1])
         else:
-            result = 0.0 if not deriv[1] else np.zeros(2)
+            result = 0.0
 
-        for node in self.bfs():
-            result += node.value * node.nodes[0].eval(
-                t, deriv[0]) * node.nodes[1].eval(x, deriv[1])
+        for mlt_node in self.bfs():
+            if mlt_node == 0.0: continue
+            result += mlt_node.value * np.prod(
+                [n.eval(c) for (n, c) in zip(mlt_node.nodes, coords)], axis=0)
         return result
+
+
+class DoubleTreeFunction(MultiTreeFunction, DoubleTreeVector):
+    pass
--- Python/datastructures/multi_tree_function_test.py
+++ Python/datastructures/multi_tree_function_test.py
@@ -1,10 +1,7 @@
 import numpy as np
 
-from ..datastructures.double_tree_vector import (DoubleNodeVector,
-                                                 DoubleTreeVector,
-                                                 FrozenDoubleNodeVector)
-from ..datastructures.double_tree_view import DoubleTree
-from ..datastructures.double_tree_function import DoubleTreeFunction
+from ..datastructures.multi_tree_function import (MultiTreeFunction,
+                                                  DoubleTreeFunction)
 from ..space.triangulation import InitialTriangulation
 from ..space.basis import HierarchicalBasisFunction
 from ..time.three_point_basis import ThreePointBasis
@@ -21,16 +18,15 @@ def test_single_nonzero():
     basis_time = ThreePointBasis()
     basis_time.metaroot_wavelet.uniform_refine(1)
 
-    tree = DoubleTree((basis_time.metaroot_wavelet, basis_space.root))
-    tree.deep_refine()
-    t, x, y = np.mgrid[0:1:3j, 0:1:3j, 0:1:3j].reshape(3, -1)
+    dbl_fn = DoubleTreeFunction.from_metaroots(
+        (basis_time.metaroot_wavelet, basis_space.root))
+    dbl_fn.uniform_refine()
+    t, x, y = np.mgrid[0:1:25j, 0:1:25j, 0:1:25j].reshape(3, -1)
     xy = np.vstack([x, y])
 
-    interpolant = tree.deep_copy(
-        mlt_node_cls=DoubleNodeVector,
-        mlt_tree_cls=DoubleTreeFunction,
-        call_postprocess=lambda nv, _: setattr(nv, 'value', 0.0))
-    interpolant.bfs()[0].value = 1.0
-    eval_interp = interpolant.eval(t, xy)
-    print("hier")
-    print(eval_interp.reshape(3, 3, 3))
+    for dbl_node in dbl_fn.bfs():
+        dbl_node.value = 1.0
+        assert np.allclose(
+            dbl_fn.eval((t, xy)),
+            dbl_node.nodes[0].eval(t) * dbl_node.nodes[1].eval(xy))
+        dbl_node.value = 0.0
