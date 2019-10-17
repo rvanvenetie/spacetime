09060e316cfcafe362b1c47e53a3d2883720bac9
Jan Westerdiep <janner@gmail.com>
2019-10-17 11:48:24 +0200

    added TreeFunction

--- Python/datastructures/multi_tree_function.py
+++ Python/datastructures/multi_tree_function.py
@@ -2,22 +2,21 @@ import numpy as np
 
 from ..datastructures.multi_tree_vector import MultiTreeVector
 from ..datastructures.double_tree_vector import DoubleTreeVector
+from ..datastructures.tree_vector import TreeVector
 
 
 class MultiTreeFunction(MultiTreeVector):
     """ Class that represents a function living on a multi tree. """
-    def __init__(self, root):
-        super().__init__(root=root)
-
     def eval(self, coords):
         """ Evaluate in a stupid way.
         
         Arguments:
-            coords: a list of coords, each a float or array of size (dim, N).
+            coords: a tuple of coords, each a float or array of size (dim, N).
 
         Returns:
             A float, or array of shape (N,).
         """
+        assert isinstance(coords, tuple)
         assert len(coords) == self.root.dim
         # If the input coords are arrays, return an array, else a number.
         if any(isinstance(coord, np.ndarray) for coord in coords):
@@ -36,3 +35,9 @@ class MultiTreeFunction(MultiTreeVector):
 
 class DoubleTreeFunction(MultiTreeFunction, DoubleTreeVector):
     pass
+
+
+class TreeFunction(MultiTreeFunction, TreeVector):
+    def eval(self, coords):
+        if not isinstance(coords, tuple): coords = (coords, )
+        return super().eval(coords)
--- Python/datastructures/multi_tree_function_test.py
+++ Python/datastructures/multi_tree_function_test.py
@@ -1,13 +1,13 @@
 import numpy as np
 
-from ..datastructures.multi_tree_function import (MultiTreeFunction,
+from ..datastructures.multi_tree_function import (TreeFunction,
                                                   DoubleTreeFunction)
 from ..space.triangulation import InitialTriangulation
 from ..space.basis import HierarchicalBasisFunction
 from ..time.three_point_basis import ThreePointBasis
 
 
-def test_single_nonzero():
+def test_dbl_fn_single_nonzero():
     # Create space part.
     triang = InitialTriangulation.unit_square()
     triang.vertex_meta_root.uniform_refine(1)
@@ -30,3 +30,15 @@ def test_single_nonzero():
             dbl_fn.eval((t, xy)),
             dbl_node.nodes[0].eval(t) * dbl_node.nodes[1].eval(xy))
         dbl_node.value = 0.0
+
+
+def test_fn_small():
+    basis_time = ThreePointBasis()
+    basis_time.metaroot_wavelet.uniform_refine(1)
+    fn = TreeFunction.from_metaroot(basis_time.metaroot_wavelet)
+    fn.uniform_refine()
+    t = np.linspace(0, 1, 1001)
+    for node in fn.bfs():
+        node.value = 1.0
+        assert np.allclose(fn.eval(t), node.node.eval(t))
+        node.value = 0.0
