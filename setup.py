bbde1abb99ee53722c53c2e13d14f0262925518a
Raymond van Venetie <rayvanve@gmail.com>
2019-10-22 20:28:58 +0200

    added few more things

--- setup.py
+++ setup.py
@@ -6,7 +6,9 @@ setup(ext_modules=cythonize(
     [
         'Python/datastructures/*[!test].py',
         'Python/space/*[!test].py',
+        'Python/spacetime/*[!test].py',
         'Python/time/*[!test].py',
+        'Python/applications/*[!test].py',
     ],
     compiler_directives={'language_level': "3"},
 ))
