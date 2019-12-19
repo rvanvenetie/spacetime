57d0c987dd54ef3d33c76572b2a9b762c610d98b
Raymond van Venetie <rayvanve@gmail.com>
2019-12-19 13:04:10 +0100

    fixed tests

--- Python/space/functional.py
+++ Python/space/functional.py
@@ -11,14 +11,14 @@ class Functional:
             singlescale_functional: functional to apply.
         """
         super().__init__()
-        self.operator = singlescale_functional
+        self.functional = singlescale_functional
 
     def eval(self, Lambda):
         """ Evaluate the functional on the given functions. """
         vec_out = TreeVector.from_metaroot(Lambda.node)
         vec_out.union(Lambda)
 
-        self.operator.triang = TriangulationView(vec_out)
-        np_vec_out = self.singlescale_functional.eval()
+        self.functional.triang = TriangulationView(vec_out)
+        np_vec_out = self.functional.eval()
         vec_out.from_array(np_vec_out)
         return vec_out
