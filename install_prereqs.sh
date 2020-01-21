f2a174ed2407fd4904cbbddd6824db310346aaaf
Raymond van Venetie <rayvanve@gmail.com>
2020-01-21 20:10:18 +0100

    added cmake

--- install_prereqs.sh
+++ install_prereqs.sh
@@ -10,3 +10,5 @@ sudo apt-get update -y
 sudo apt-get install gcc-7 g++-7 libboost-1.70-dev eigen3 -y
 sudo update-alternatives --install /usr/bin/gcc gcc /usr/bin/gcc-7 60 --slave /usr/bin/g++ g++ /usr/bin/g++-7
 sudo update-alternatives --config gcc
+
+pip3 install cmake
