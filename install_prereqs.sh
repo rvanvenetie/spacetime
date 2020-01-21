c2d8802bd3f05793377b140c2eaedf1c31149fe6
Raymond van Venetie <rayvanve@gmail.com>
2020-01-21 21:18:59 +0100

    install pip

--- install_prereqs.sh
+++ install_prereqs.sh
@@ -6,7 +6,7 @@ sudo add-apt-repository ppa:ubuntu-toolchain-r/test -y
 sudo add-apt-repository ppa:mhier/libboost-latest -y
 sudo add-apt-repository ppa:nschloe/eigen-backports -y
 
-install-package gcc-9 g++-9 libboost1.70-dev libeigen3-dev
+install-package gcc-9 g++-9 libboost1.70-dev libeigen3-dev python-pip
 sudo update-alternatives --install /usr/bin/gcc gcc /usr/bin/gcc-9 130 --slave /usr/bin/g++ g++ /usr/bin/g++-9
 
 pip install cmake
