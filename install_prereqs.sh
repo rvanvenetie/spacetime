24105d15640d1f6cfc906556b82e412638b9450d
Raymond van Venetie <rayvanve@gmail.com>
2020-01-21 21:26:33 +0100

    sudo pip install

--- install_prereqs.sh
+++ install_prereqs.sh
@@ -9,6 +9,6 @@ sudo add-apt-repository ppa:nschloe/eigen-backports -y
 install-package gcc-9 g++-9 libboost1.70-dev libeigen3-dev python-pip
 sudo update-alternatives --install /usr/bin/gcc gcc /usr/bin/gcc-9 130 --slave /usr/bin/g++ g++ /usr/bin/g++-9
 
-pip install cmake
+sudo pip install cmake
 
 cmake -S C++ -B C++/build
