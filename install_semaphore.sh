38e5bf4d1d753eba3970a52794f159970b990aa4
Raymond van Venetie <rayvanve@gmail.com>
2020-01-22 15:04:20 +0100

    try with very old eigen

--- install_semaphore.sh
+++ install_semaphore.sh
@@ -3,9 +3,9 @@ sudo apt-get purge cmake cmake-data 'libboost.*'  -y
 
 sudo add-apt-repository ppa:ubuntu-toolchain-r/test -y
 sudo add-apt-repository ppa:mhier/libboost-latest -y
-sudo add-apt-repository ppa:nschloe/eigen-backports -y
+# sudo add-apt-repository ppa:nschloe/eigen-backports -y
 
-install-package --update gcc-9 g++-9 libboost1.70-dev libeigen3-dev python-pip
+install-package --update gcc-9 g++-9 libboost1.70-dev python-pip
 sudo update-alternatives --install /usr/bin/gcc gcc /usr/bin/gcc-9 130 --slave /usr/bin/g++ g++ /usr/bin/g++-9
 
 sudo pip install cmake
