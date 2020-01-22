f270a6d9fb22811de908882b8f6301485ddffce7
Raymond van Venetie <rayvanve@gmail.com>
2020-01-22 15:07:56 +0100

    oke just keep this working plx

--- install_semaphore.sh
+++ install_semaphore.sh
@@ -3,7 +3,7 @@ sudo apt-get purge cmake cmake-data 'libboost.*'  -y
 
 sudo add-apt-repository ppa:ubuntu-toolchain-r/test -y
 sudo add-apt-repository ppa:mhier/libboost-latest -y
-# sudo add-apt-repository ppa:nschloe/eigen-backports -y
+sudo add-apt-repository ppa:nschloe/eigen-backports -y
 
 install-package --update gcc-9 g++-9 libboost1.70-dev python-pip libeigen3-dev
 sudo update-alternatives --install /usr/bin/gcc gcc /usr/bin/gcc-9 130 --slave /usr/bin/g++ g++ /usr/bin/g++-9
