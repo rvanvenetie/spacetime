8e45cad2663c22878e6ea86b1bfd0e2008207b76
Jan Westerdiep <janner@gmail.com>
2020-01-27 13:59:33 +0100

    first draft

---
+++ C++/time/bilinear_form_performance.cpp
@@ -0,0 +1,49 @@
+#include <cmath>
+#include <map>
+#include <random>
+#include <set>
+
+#include "time/basis.hpp"
+#include "time/bilinear_form.hpp"
+
+int bsd_rnd() {
+  static unsigned int seed = 0;
+  int a = 1103515245;
+  int c = 12345;
+  unsigned int m = 2147483648;
+  return (seed = (a * seed + c) % m);
+}
+
+using namespace Time;
+using namespace datastructures;
+
+constexpr int level = 10;
+constexpr int bilform_iters = 10;
+constexpr int inner_iters = 150;
+
+int main() {
+  three_point_tree.UniformRefine(::level);
+
+  for (size_t j = 0; j < ::bilform_iters; ++j) {
+    // Set up three-point tree.
+    auto view_in = TreeView<ThreePointWaveletFn>(three_point_tree.meta_root);
+    auto vec_out = TreeVector<ThreePointWaveletFn>(three_point_tree.meta_root);
+    view_in.DeepRefine(
+        /* call_filter */ [](auto&& nv) {
+          return nv->level() <= 0 || bsd_rnd() % 3 != 0;
+        });
+    vec_out.DeepRefine(
+        /* call_filter */ [](auto&& nv) {
+          return nv->level() <= 0 || bsd_rnd() % 3 != 0;
+        });
+
+    BilinearForm<MassOperator, ThreePointWaveletFn, ThreePointWaveletFn>
+        bil_form(&vec_out);
+    for (size_t k = 0; k < ::inner_iters; k++) {
+      auto vec_in = view_in.template DeepCopy<
+          datastructures::TreeVector<ThreePointWaveletFn>>(
+          [](auto nv, auto _) { nv->set_value(1.0); });
+      bil_form.Apply(vec_in);
+    }
+  }
+}
---
+++ Python/time/applicator_performance.py
@@ -0,0 +1,34 @@
+import numpy as np
+
+from ..datastructures.tree_view import TreeView
+from . import operators
+from .applicator import Applicator
+from .sparse_vector import SparseVector
+from .three_point_basis import ThreePointBasis
+
+seed = 0
+
+
+def bsd_rnd():
+    global seed
+    seed = (1103515245 * seed + 12345) & 0x7fffffff
+    return seed
+
+
+def test_python(level, bilform_iters, inner_iters):
+    basis, _ = ThreePointBasis.uniform_basis(max_level=level)
+
+    for _ in range(bilform_iters):
+        vec = TreeView.from_metaroot(ThreePointBasis.metaroot_wavelet)
+        vec.deep_refine(
+            call_filter=lambda fn: fn.level <= 0 or (bsd_rnd() % 3) != 0)
+        Lambda = [node.node for node in vec.bfs()]
+        applicator = Applicator(operators.mass(basis), basis)
+        for _ in range(inner_iters):
+            np_vec = np.ones(len(Lambda))
+            vec = SparseVector(Lambda, np_vec)
+            applicator.apply(vec)
+
+
+if __name__ == "__main__":
+    test_python(level=10, bilform_iters=10, inner_iters=150)
