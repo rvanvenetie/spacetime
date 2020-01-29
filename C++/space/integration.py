cc85e36104305ce2938b5942054229e70c3a792e
Jan Westerdiep <janner@gmail.com>
2020-01-29 16:12:46 +0100

    small change

--- C++/space/integration.py
+++ C++/space/integration.py
@@ -1,16 +1,31 @@
+""" Usage: python3 integration.py > integration.ipp """
 import quadpy
 
-print("namespace space {")
+print("""#pragma once
+
+#include "integration.hpp"
+
+namespace space {
+template <unsigned P>
+template <class F>
+double IntegrationRule<P>::Integrate(F f, const Element2D &elem) {
+  double integral = 0.0;
+  for (const auto [p, q, w] : rule) {
+    auto [x, y] = elem.GlobalCoordinates(p, q);
+    integral += w * f(x, y);
+  }
+  return elem.area() * integral;
+}
+""")
 for order in range(10):
     nco = quadpy.triangle.newton_cotes_open(order)
     npoints = len(nco.weights)
     print("template <>")
     print("std::vector<std::tuple<double, double, double>>"
-          " IntegrationRule<%d>::rule{" % order,
-          end='')
+          " IntegrationRule<%d>::rule{" % order)
     for p in range(npoints):
-        print("\n\t{%.20f,%.20f,%.20f, %d}," %
-              (nco.points[p][1], nco.points[p][2], nco.weights[p], nco.degree),
-              end='')
+        print(
+            "\t\t{%.20f, %.20f, %.20f}," %
+            (nco.points[p][1], nco.points[p][2], nco.weights[p]), )
     print("};")
 print("};  // namespace space")
