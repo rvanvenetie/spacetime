7a81b4d6a7a139401e2219c486cd1ac2ffea684c
Jan Westerdiep <janner@gmail.com>
2020-01-30 16:47:09 +0100

    added faulty cmake shit

--- C++/tools/integration_test.cpp
+++ C++/tools/integration_test.cpp
@@ -10,27 +10,15 @@ using datastructures::static_for;
 
 namespace tools {
 TEST(Integration1D, ProductOfMonomials) {
-  auto T = InitialTriangulation::UnitSquare();
-  T.elem_tree.UniformRefine(4);
-
-  static_for<10>([&T](auto order) {
-    for (auto elem : T.elem_tree.Bfs()) {
-      auto f = [](double x, double y) { return 1.0; };
-      EXPECT_NEAR(IntegrationRule<order>::Integrate(f, *elem), elem->area(),
-                  1e-10);
-    }
-  });
+  time::elem_tree.UniformRefine(4);
 
   static_for<10>([&T](auto degree) {
     auto root = T.elem_tree.Bfs()[0];
     for (size_t n = 0; n < degree; n++) {
-      for (size_t m = 0; m + n <= degree; m++) {
-        auto f = [n, m](double x, double y) { return pow(x, n) * pow(y, m); };
-        EXPECT_NEAR(
-            IntegrationRule<degree>::Integrate(f, *root),
-            tgamma(2 + m) * tgamma(1 + n) / ((1 + m) * tgamma(3 + m + n)),
-            1e-10);
-      }
+      auto f = [n](double x) { return pow(x, n); };
+      auto [a, b] = elem->Interval();
+      EXPECT_NEAR(IntegrationRule<1, degree>::Integrate(f, *root),
+                  (pow(b, n) - pow(a, n)) / (n + 1), 1e-10);
     }
   });
 }
@@ -39,10 +27,10 @@ TEST(Integration2D, ProductOfMonomials) {
   auto T = InitialTriangulation::UnitSquare();
   T.elem_tree.UniformRefine(4);
 
-  static_for<10>([&T](auto order) {
+  static_for<10>([&T](auto degree) {
     for (auto elem : T.elem_tree.Bfs()) {
       auto f = [](double x, double y) { return 1.0; };
-      EXPECT_NEAR(IntegrationRule<order>::Integrate(f, *elem), elem->area(),
+      EXPECT_NEAR(IntegrationRule<2, degree>::Integrate(f, *elem), elem->area(),
                   1e-10);
     }
   });
@@ -53,7 +41,7 @@ TEST(Integration2D, ProductOfMonomials) {
       for (size_t m = 0; m + n <= degree; m++) {
         auto f = [n, m](double x, double y) { return pow(x, n) * pow(y, m); };
         EXPECT_NEAR(
-            IntegrationRule<degree>::Integrate(f, *root),
+            IntegrationRule<2, degree>::Integrate(f, *root),
             tgamma(2 + m) * tgamma(1 + n) / ((1 + m) * tgamma(3 + m + n)),
             1e-10);
       }
