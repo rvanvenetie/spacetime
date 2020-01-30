c02e318f016a258e30954220a29954972e79c14c
Jan Westerdiep <janner@gmail.com>
2020-01-30 17:00:58 +0100

    af

--- C++/tools/integration_test.cpp
+++ C++/tools/integration_test.cpp
@@ -7,18 +7,22 @@
 #include "gtest/gtest.h"
 
 using datastructures::static_for;
+using space::InitialTriangulation;
+using Time::elem_tree;
 
 namespace tools {
-TEST(Integration1D, ProductOfMonomials) {
-  time::elem_tree.UniformRefine(4);
+TEST(Integration1D, Monomials) {
+  elem_tree.UniformRefine(4);
 
-  static_for<10>([&T](auto degree) {
-    auto root = T.elem_tree.Bfs()[0];
-    for (size_t n = 0; n < degree; n++) {
-      auto f = [n](double x) { return pow(x, n); };
-      auto [a, b] = elem->Interval();
-      EXPECT_NEAR(IntegrationRule<1, degree>::Integrate(f, *root),
-                  (pow(b, n) - pow(a, n)) / (n + 1), 1e-10);
+  static_for<10>([&](auto degree) {
+    for (auto elem : elem_tree.Bfs()) {
+      for (size_t n = 0; n < degree; n++) {
+        auto f = [n](double x) { return pow(x, n); };
+        auto [a, b] = elem->Interval();
+        auto result = IntegrationRule<1, degree>::Integrate(f, *elem);
+        auto expected = (pow(b, n + 1) - pow(a, n + 1)) / (n + 1);
+        EXPECT_NEAR(result, expected, 1e-10);
+      }
     }
   });
 }
@@ -30,8 +34,8 @@ TEST(Integration2D, ProductOfMonomials) {
   static_for<10>([&T](auto degree) {
     for (auto elem : T.elem_tree.Bfs()) {
       auto f = [](double x, double y) { return 1.0; };
-      EXPECT_NEAR(IntegrationRule<2, degree>::Integrate(f, *elem), elem->area(),
-                  1e-10);
+      auto result = IntegrationRule<2, degree>::Integrate(f, *elem);
+      EXPECT_NEAR(result, elem->area(), 1e-10);
     }
   });
 
@@ -40,10 +44,10 @@ TEST(Integration2D, ProductOfMonomials) {
     for (size_t n = 0; n < degree; n++) {
       for (size_t m = 0; m + n <= degree; m++) {
         auto f = [n, m](double x, double y) { return pow(x, n) * pow(y, m); };
-        EXPECT_NEAR(
-            IntegrationRule<2, degree>::Integrate(f, *root),
-            tgamma(2 + m) * tgamma(1 + n) / ((1 + m) * tgamma(3 + m + n)),
-            1e-10);
+        auto result = IntegrationRule<2, degree>::Integrate(f, *root);
+        auto expected =
+            tgamma(2 + m) * tgamma(1 + n) / ((1 + m) * tgamma(3 + m + n));
+        EXPECT_NEAR(result, expected, 1e-10);
       }
     }
   });
