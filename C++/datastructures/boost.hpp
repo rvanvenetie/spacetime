c3a96fbf1adeac494a70211cf135c6d7e8a2a214
Raymond van Venetie <rayvanve@gmail.com>
2020-02-10 11:33:43 +0100

    remove boost deque

--- C++/datastructures/boost.hpp
+++ C++/datastructures/boost.hpp
@@ -9,9 +9,9 @@ using SmallVector = boost::container::small_vector<I, N>;
 template <typename I, size_t N>
 using StaticVector = boost::container::static_vector<I, N>;
 
-// Boost deque container with default block size N.
-template <typename I, size_t N = 128>
-using BoostDeque =
-    boost::container::deque<I, void,
-                            typename boost::container::deque_options<
-                                boost::container::block_size<N>>::type>;
+// Boost deque container with default block size N. (REQUIRES LATEST BOOST).
+// template <typename I, size_t N = 128>
+// using BoostDeque =
+//     boost::container::deque<I, void,
+//                             typename boost::container::deque_options<
+//                                 boost::container::block_size<N>>::type>;
