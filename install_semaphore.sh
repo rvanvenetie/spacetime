13a96d06a8fc7a1c8c31e7b620cb9a9a506724b5
Ubuntu User <ubuntu@packer-Ubuntu-18.04.1-Server>
2020-02-14 12:13:48 +0100

    poging 2

--- install_semaphore.sh
+++ install_semaphore.sh
@@ -6,12 +6,12 @@ sudo apt-get purge cmake cmake-data  -y
 # Add repository for the latest gcc.
 sudo add-apt-repository ppa:ubuntu-toolchain-r/test -y
 
-# Install latest gcc.
-install-package --update gcc-9 g++-9 snapd
+# Install latest gcc & pip.
+install-package --update gcc-9 g++-9 python-pip
 sudo update-alternatives --install /usr/bin/gcc gcc /usr/bin/gcc-9 130 --slave /usr/bin/g++ g++ /usr/bin/g++-9
 
 # Install latest cmake.
-sudo snap install cmake --classic
+sudo pip install cmake
 
 # Build.
 cmake -S C++ -B C++/build
