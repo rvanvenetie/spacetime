5e59971d5f39c209739045fb1f3c9e20762ed311
Jan Westerdiep <janner@gmail.com>
2020-02-27 13:36:38 +0100

    removed comment

--- C++/datastructures/bilinear_form.hpp
+++ C++/datastructures/bilinear_form.hpp
@@ -59,7 +59,6 @@ struct generic_product_impl<EigenBilinearForm, Rhs, SparseShape, DenseShape,
   static void scaleAndAddTo(Dest &dst, const EigenBilinearForm &lhs,
                             const Rhs &rhs, const Scalar &alpha) {
     assert(alpha == Scalar(1));
-    // dst.noalias() += const_cast<EigenBilinearForm &>(lhs).MatVec(rhs);
     dst.noalias() += lhs.MatVec(rhs);
   }
 };
