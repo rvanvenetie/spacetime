2d5f001a1882f8f5ed13b1d7ddb6ffe00754be4e
Jan Westerdiep <janner@gmail.com>
2020-03-17 13:07:45 +0100

    adaptive loop

---
+++ C++/applications/adaptive.cpp
@@ -0,0 +1,77 @@
+#include "../space/initial_triangulation.hpp"
+#include "../spacetime/linear_form.hpp"
+#include "../time/basis.hpp"
+#include "../tools/linalg.hpp"
+#include "adaptive_heat_equation.hpp"
+
+using applications::AdaptiveHeatEquation;
+using datastructures::DoubleTreeVector;
+using datastructures::DoubleTreeView;
+using space::HierarchicalBasisFn;
+using spacetime::CreateQuadratureLinearForm;
+using spacetime::CreateSumLinearForm;
+using spacetime::CreateZeroEvalLinearForm;
+using spacetime::GenerateYDelta;
+using Time::ortho_tree;
+using Time::OrthonormalWaveletFn;
+using Time::three_point_tree;
+using Time::ThreePointWaveletFn;
+
+#include <mach/mach.h>
+#include <mach/task.h>
+
+std::pair<unsigned long long, unsigned long long> getmem() {
+  task_t task = MACH_PORT_NULL;
+  struct task_basic_info t_info;
+  mach_msg_type_number_t t_info_count = TASK_BASIC_INFO_COUNT;
+
+  assert(KERN_SUCCESS == task_info(mach_task_self(), TASK_BASIC_INFO,
+                                   (task_info_t)&t_info, &t_info_count));
+  return {t_info.resident_size, t_info.virtual_size};
+}
+
+auto SmoothProblem() {
+  // Solution u = (1 + t^2) x (1-x) y (1-y).
+  auto time_g1 = [](double t) { return -2 * (1 + t * t); };
+  auto space_g1 = [](double x, double y) { return (x - 1) * x + (y - 1) * y; };
+  auto time_g2 = [](double t) { return 2 * t; };
+  auto space_g2 = [](double x, double y) { return (x - 1) * x * (y - 1) * y; };
+  auto u0 = [](double x, double y) { return (1 - x) * x * (1 - y) * y; };
+
+  return std::make_pair(
+      CreateSumLinearForm<OrthonormalWaveletFn>(
+          CreateQuadratureLinearForm<OrthonormalWaveletFn, 2, 2>(time_g1,
+                                                                 space_g1),
+          CreateQuadratureLinearForm<OrthonormalWaveletFn, 1, 4>(time_g2,
+                                                                 space_g2)),
+      CreateZeroEvalLinearForm<ThreePointWaveletFn, 4>(u0));
+}
+
+int main() {
+  auto T = space::InitialTriangulation::UnitSquare();
+  T.hierarch_basis_tree.UniformRefine(1);
+  ortho_tree.UniformRefine(1);
+  three_point_tree.UniformRefine(1);
+
+  auto X_delta = DoubleTreeView<ThreePointWaveletFn, HierarchicalBasisFn>(
+      three_point_tree.meta_root.get(), T.hierarch_basis_tree.meta_root.get());
+  X_delta.SparseRefine(1);
+
+  auto [g_lf, u0_lf] = SmoothProblem();
+
+  AdaptiveHeatEquation heat_eq(std::move(X_delta), std::move(g_lf),
+                               std::move(u0_lf));
+
+  while (true) {
+    auto solution = heat_eq.Solve(heat_eq.vec_Xd_out()->ToVectorContainer());
+    auto [residual, residual_norm] = heat_eq.Estimate(/*mean_zero*/ false);
+    std::cout << solution->container().size() << " " << residual_norm << " "
+              << (float)getmem().first / solution->container().size()
+              << std::endl;
+    auto marked_nodes = heat_eq.Mark();
+    heat_eq.Refine(marked_nodes);
+    // if (solution->container().size() > 1e5) break;
+  }
+
+  return 0;
+}
