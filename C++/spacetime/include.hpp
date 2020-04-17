b3f2fe5373889ae95663997b59093acd9729f050
Raymond van Venetie <rayvanve@gmail.com>
2020-04-17 17:00:11 +0200

    with all the includes

---
+++ C++/spacetime/include.hpp
@@ -0,0 +1,8 @@
+#pragma once
+#include "space/include.hpp"
+#include "time/include.hpp"
+
+#include "basis.hpp"
+#include "bilinear_form.hpp"
+#include "bilinear_form_linalg.hpp"
+#include "linear_form.hpp"
