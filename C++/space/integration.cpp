96123ef9c5157e1a5056ce8d0e848b944801d554
Jan Westerdiep <janner@gmail.com>
2020-04-20 09:33:10 +0200

    fix typo

--- C++/space/integration.cpp
+++ C++/space/integration.cpp
@@ -7,7 +7,7 @@ namespace {
 namespace space {
 double Integrate(const std::function<double(double, double)>& f,
                  const Element2D& elem, size_t degree) {
-  assert(degree <= integration_rules.size());
+  assert(degree < integration_rules.size());
   double integral = 0.0;
   for (const auto [p, q, w] : integration_rules[degree]) {
     auto [x, y] = elem.GlobalCoordinates(p, q);
--- C++/time/integration.cpp
+++ C++/time/integration.cpp
@@ -6,7 +6,7 @@ namespace {
 namespace Time {
 double Integrate(const std::function<double(double)>& f, const Element1D& elem,
                  size_t degree) {
-  assert(degree <= integration_rules.size());
+  assert(degree < integration_rules.size());
   double integral = 0.0;
   for (const auto [p, w] : integration_rules[degree]) {
     auto x = elem.GlobalCoordinates(p);
