ddc9f147f3286b3ae9b9523211ad28a6a68f5da0
Raymond van Venetie <rayvanve@gmail.com>
2020-04-25 14:50:48 +0200

    forgot includes

---
+++ C++/spacetime/includes.hpp
@@ -0,0 +1,9 @@
+// clang-format off
+#pragma once
+#include "time/bases.hpp"
+#include "time/bilinear_form.hpp"
+#include "space/basis.hpp"
+#include "space/bilinear_form.hpp"
+#include "datastructures/double_tree_view.hpp"
+#include "bilinear_form_linalg.hpp"
+// clang-format on
